c0e98803ac819b25e24a25b3d73dce0bfcff49ee
VeberAL <aleo83@rambler.ru>
2021-11-29 22:22:28 +0300

    -

--- src/test/java/VectorTest.java
+++ src/test/java/VectorTest.java
@@ -8,4 +8,9 @@ class VectorTest {
         Vector vector = new Vector();
         Assert.assertEquals(0,vector.length(),1e-6);
     }
+    @Test
+    public void newVectorShouldHaveZeroX(){
+        Vector vector = new Vector();
+        Assert.assertEquals(0,vector.getX(),1e-6);
+    }
 }
